103b4358e2edd8b5e8bbf34c0a00e315e3e26c28
Gabrielkempp <gabrielrkempp@gmail.com>
2024-11-10 16:49:33 -0300

    bugs

--- Informações_Gerais.py
+++ Informações_Gerais.py
@@ -156,4 +156,3 @@ st.sidebar.divider()
 
 st.sidebar.write('Fotos e vídeos da sua propriedade')
 botao_fotos = st.sidebar.link_button("Visualizar", "https://drive.google.com/drive/folders/1LetX9FOQ1ZSQJUd-tle4jpotKqDxwH_v?usp=sharing")
-
